7feb64b4352a48eb54af6388befce04f156002f3
Maksim Bondarenko <m.bondarenko1988@yandex.ru>
2023-06-16 00:31:47 +0300

    Done homework to five seminar about Java

---
+++ service/Service.java
@@ -0,0 +1,16 @@
+package service;
+
+import java.util.List;
+
+import model.Student;
+import model.Teacher;
+
+public interface Service {
+    
+    public void create(List<Student> inputList, Teacher teacher);
+
+    public void create(Student inputStudent);
+
+    public void read();
+
+}
